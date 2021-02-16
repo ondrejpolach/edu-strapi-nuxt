b8a2dbeca5ad10e8da48df667a0252b4d0e0572f
Ondřej Polách <polach@ekonom-system.cz>
2021-02-16 11:17:29 +0100

    reset

--- strapi/config/server.js
+++
@@ -1,9 +0,0 @@
-module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'f4f3b6d548bc79d30e2f587780c2f2b1'),
-    },
-  },
-});
--- strapi/extensions/users-permissions/config/jwt.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  jwtSecret: process.env.JWT_SECRET || '67f7c67d-71c9-46c5-900c-cfad454015a4'
-};
