a0ed48efe8bb1a13fa0b8b869aab151e7fc28d61
Ondřej Polách <polach@ekonom-system.cz>
2021-02-16 11:33:40 +0100

    nuxt and strapi added

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '2458ce370a86e7dc22985e8d52c5919e'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '6883f0b2-fd7a-4a3c-bf19-de3cd2df17a8'
+};
